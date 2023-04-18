8bbab99c2b9a5f09dfe61f4f4f47705d042bcdfb
AlWestwood1 <zceeawe@ucl.ac.uk>
2023-04-18 13:57:36 -0400

    Added ip flag system + debugging

--- dos_detection.py
+++ dos_detection.py
@@ -3,13 +3,14 @@ import time
 from scapy.all import *
 from collections import deque
 
-THRESHOLD = 1000
+THRESHOLD = 5000
 SLIDING_WINDOW = 30
 pkt_counter = {}
 
 pkt_timestamps = deque()
 
 blocked_ips = set()
+flagged_ips = set()
 
 logs = open("dos_detection.log", 'a')
 
@@ -18,7 +19,7 @@ def packet_handling(pkt):
         src = pkt[IP].src
         pkt_timestamps.append(time.time())
 
-        while len(pkt_timestamps > 0) and pkt_timestamps[0] < time.time() - SLIDING_WINDOW:
+        while pkt_timestamps and pkt_timestamps[0] < time.time() - SLIDING_WINDOW:
             pkt_timestamps.popleft()
 
         if src in pkt_counter:
@@ -27,18 +28,28 @@ def packet_handling(pkt):
         else:
             pkt_counter[src] = 1
 
-        if pkt_counter[src] > THRESHOLD:
+        if pkt_counter[src] > THRESHOLD and src not in flagged_ips:
             print("Potential DoS attack detected from:", src)
             valid = False
             while not valid:
                 block = str(input("Would you like to block this IP address? (Y/N): ")).lower()
-                if block == 'Y':
+                if block == 'y':
                     valid = True
-                    logs.write("Blocked IP:", src + "\n")
+                    logs.write("Blocked IP: "+ src + "\n")
+                    print("Blocked IP: "+ src + "\n")
                     #os.system("iptables -A INPUT -s " + src + "-j DROP")
                     blocked_ips.add(src)
+                    flagged_ips.add(src)
+                elif block == 'n':
+                    valid = True
+                    flagged_ips.add(src)
+                else:
+                    print("Invalid input, please try again.\n")
 
 
-def main():
-    interface = "en0"
-    sniff(filter="ip", prn=packet_handling, iface=interface)
+
+interface = "wlan0"
+print("Monitoring traffic...")
+sniff(filter="ip", prn=packet_handling, iface=interface)
+    
+
