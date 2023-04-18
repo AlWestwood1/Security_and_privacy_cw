dbb8c55cc4dc0e2c79bb8af4f1c678e65d5046a5
AlWestwood1 <zceeawe@ucl.ac.uk>
2023-04-18 20:50:18 +0100

    Added port scanning functionality + comments

--- dos_detection.py
+++ dos_detection.py
@@ -3,41 +3,45 @@ import time
 from scapy.all import *
 from collections import deque
 
-THRESHOLD = 5000
-SLIDING_WINDOW = 30
+THRESHOLD = 5000 # Number of packets before a potential attack is flagged
+SLIDING_WINDOW = 30 # Time in seconds where packets will be counted
+
+#Initialise variables
 pkt_counter = {}
-
 pkt_timestamps = deque()
+blocked_ips = set() #List of blocked ips
+flagged_ips = set() #List of any ips that have been notified to the user - they may have decided not to block it
 
-blocked_ips = set()
-flagged_ips = set()
-
+#Open log file
 logs = open("detection_logs.log", 'a')
 
 def packet_handling(pkt):
-    if IP in pkt:
+    if IP in pkt: #Check that packet is an IP packet
         src = pkt[IP].src
-        pkt_timestamps.append(time.time())
+        pkt_timestamps.append(time.time()) #Add current time to the timestamp queue
 
-        while pkt_timestamps and pkt_timestamps[0] < time.time() - SLIDING_WINDOW:
+        #Remove timestamps that are out of the sliding window range
+        while pkt_timestamps and pkt_timestamps[0] < time.time() - SLIDING_WINDOW: 
             pkt_timestamps.popleft()
 
+        #Count how many times a valid packet has come from the same source in the sliding window range
         if src in pkt_counter:
             pkt_counter[src] = sum(1 for t in pkt_timestamps if time.time())
 
+        #If it's a new source initialise dictionary value to 1
         else:
             pkt_counter[src] = 1
 
+        #Notify client if the number of packets goes above the threshold (and if not alredy been flagged)
         if pkt_counter[src] > THRESHOLD and src not in flagged_ips:
             print("Potential DoS attack detected from:", src)
             valid = False
-            while not valid:
+            while not valid: #Blocking system
                 block = str(input("Would you like to block this IP address? (Y/N): ")).lower()
                 if block == 'y':
                     valid = True
                     logs.write("Blocked IP: "+ src + "\n")
                     print("Blocked IP: "+ src + "\n")
-                    #os.system("iptables -A INPUT -s " + src + "-j DROP")
                     blocked_ips.add(src)
                     flagged_ips.add(src)
                 elif block == 'n':
--- port_scan_detection.py
+++ port_scan_detection.py
@@ -3,51 +3,54 @@ from collections import defaultdict
 from scapy.all import *
 
 
-THRESHOLD = 10
-TIME_WINDOW = 10
+THRESHOLD = 10 #Number of different ports connected to by a single source before being flagged
+TIME_WINDOW = 10 #Time window in second to count the ports visited
 
-blocked_ips = set()
-flagged_ips = set()
+blocked_ips = set() #List of blocked ips
+flagged_ips = set() #List of any ips that have been notified to the user - they may have decided not to block it
 
-connections = defaultdict(list)
-pkt_timestamps = defaultdict(list)
+connections = defaultdict(list) #All ports the source ip has connected to
+pkt_timestamps = defaultdict(list) #Timestamps of the connections
 
-logs = open("detection_logs.log", 'a')
+logs = open("detection_logs.log", 'a') #Log file
 
 def port_scan_detection(pkt):
-    if pkt.haslayer(TCP):
-        if pkt[TCP].flags == "S":
-            src = pkt[IP].src
-            dport = pkt[TCP].dport
-            
-            if src in connections:
-                ports = connections[src]
+    #Check if the packet is a SYN packet
+    if pkt.haslayer(TCP) and pkt[TCP].flags == "S":
+        src = pkt[IP].src
+        dport = pkt[TCP].dport
+        
+        #Check if the source ip has been detected before
+        if src in connections:
+            ports = connections[src]
 
-                if dport not in ports:
-                    ports.append(dport)
-                
-                if len(ports) > THRESHOLD:
-                    if TIME_WINDOW > time.time() - pkt_timestamps[src][0]:
-                        print("Potential port scanner detected", src)
-                        while not valid:
-                            block = str(input("Would you like to block this IP address? (Y/N): ")).lower()
-                            if block == 'y':
-                                valid = True
-                                logs.write("Blocked IP: "+ src + "\n")
-                                print("Blocked IP: "+ src + "\n")
-                                #os.system("iptables -A INPUT -s " + src + "-j DROP")
-                                blocked_ips.add(src)
-                                flagged_ips.add(src)
-                            elif block == 'n':
-                                valid = True
-                                flagged_ips.add(src)
-                            else:
-                                print("Invalid input, please try again.\n")
+            #Append any new ports to the connections list
+            if dport not in ports:
+                ports.append(dport)
             
-            else:
+            #Check if the number of ports is above the threshold within the given time window (and has not already been flagged)
+            if len(ports) > THRESHOLD and TIME_WINDOW > time.time() - pkt_timestamps[src][0] and src not in flagged_ips:
+                print("Potential port scanner detected", src)
+                valid = False
+                while not valid: #Blocking process
+                    block = str(input("Would you like to block this IP address? (Y/N): ")).lower()
+                    if block == 'y':
+                        valid = True
+                        logs.write("Blocked IP: "+ src + "\n")
+                        print("Blocked IP: "+ src + "\n")
+                        blocked_ips.add(src) #Add to blocked list
+                        flagged_ips.add(src) #Add to flagged list
+                    elif block == 'n':
+                        valid = True
+                        flagged_ips.add(src) #Add to flagged list
+                    else:
+                        print("Invalid input, please try again.\n")
+            
+            else: #If the source is new:
                 connections[src] = [dport]
                 pkt_timestamps[src] = [time.time()]
 
 if __name__ == '__main__':
     interface = "wlan0"
+    print("Monitoring traffic...")
     sniff(filter="tcp", prn=port_scan_detection, iface=interface)
