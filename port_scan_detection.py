2833fe5c7f7fc12944914598a4f26b6f364abe39
AlWestwood1 <zceeawe@ucl.ac.uk>
2023-04-18 21:01:26 +0100

    debugging

--- port_scan_detection.py
+++ port_scan_detection.py
@@ -16,39 +16,40 @@ logs = open("detection_logs.log", 'a') #Log file
 
 def port_scan_detection(pkt):
     #Check if the packet is a SYN packet
-    if pkt.haslayer(TCP) and pkt[TCP].flags == "S":
-        src = pkt[IP].src
-        dport = pkt[TCP].dport
-        
-        #Check if the source ip has been detected before
-        if src in connections:
-            ports = connections[src]
+    if pkt.haslayer(TCP):
+        if pkt[TCP].flags == "S":
+            src = pkt[IP].src
+            dport = pkt[TCP].dport
+            
+            #Check if the source ip has been detected before
+            if src in connections:
+                ports = connections[src]
 
-            #Append any new ports to the connections list
-            if dport not in ports:
-                ports.append(dport)
-            
-            #Check if the number of ports is above the threshold within the given time window (and has not already been flagged)
-            if len(ports) > THRESHOLD and TIME_WINDOW > time.time() - pkt_timestamps[src][0] and src not in flagged_ips:
-                print("Potential port scanner detected", src)
-                valid = False
-                while not valid: #Blocking process
-                    block = str(input("Would you like to block this IP address? (Y/N): ")).lower()
-                    if block == 'y':
-                        valid = True
-                        logs.write("Blocked IP: "+ src + "\n")
-                        print("Blocked IP: "+ src + "\n")
-                        blocked_ips.add(src) #Add to blocked list
-                        flagged_ips.add(src) #Add to flagged list
-                    elif block == 'n':
-                        valid = True
-                        flagged_ips.add(src) #Add to flagged list
-                    else:
-                        print("Invalid input, please try again.\n")
-            
-            else: #If the source is new:
-                connections[src] = [dport]
-                pkt_timestamps[src] = [time.time()]
+                #Append any new ports to the connections list
+                if dport not in ports:
+                    ports.append(dport)
+                
+                #Check if the number of ports is above the threshold within the given time window (and has not already been flagged)
+                if len(ports) > THRESHOLD and TIME_WINDOW > time.time() - pkt_timestamps[src][0] and src not in flagged_ips:
+                    print("Potential port scanner detected", src)
+                    valid = False
+                    while not valid: #Blocking process
+                        block = str(input("Would you like to block this IP address? (Y/N): ")).lower()
+                        if block == 'y':
+                            valid = True
+                            logs.write("Blocked IP: "+ src + "\n")
+                            print("Blocked IP: "+ src + "\n")
+                            blocked_ips.add(src) #Add to blocked list
+                            flagged_ips.add(src) #Add to flagged list
+                        elif block == 'n':
+                            valid = True
+                            flagged_ips.add(src) #Add to flagged list
+                        else:
+                            print("Invalid input, please try again.\n")
+                
+                else: #If the source is new:
+                    connections[src] = [dport]
+                    pkt_timestamps[src] = [time.time()]
 
 if __name__ == '__main__':
     interface = "wlan0"
